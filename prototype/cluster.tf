4a59a9b6e1a0a6b4ce3bdb2b52bcaffc7e9fa665
John Oberly III <joberly@gmail.com>
2020-02-11 18:23:20 -0600

    prototype: new cluster

---
+++ prototype/cluster.tf
@@ -0,0 +1,19 @@
+provider "digitalocean" {
+    token = "${var.do_api_token}"
+}
+
+resource "digitalocean_kubernetes_cluster" "prototype" {
+    name = "${var.do_cluster_name}"
+    region = "sfo2"
+    version = "1.16.6-do.0"
+
+    node_pool {
+        name = "worker-pool"
+        size = "s-1vcpu-2gb"
+        node_count = 1
+    }
+}
+
+output "cluster-id" {
+    value = "${digitalocean_kubernetes_cluster.prototype.id}"
+}
---
+++ prototype/values.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Source this file before running Terraform locally.
+
+export TF_VAR_do_cluster_name="prototype"
+export TF_VAR_do_api_token="$DO_API_TOKEN"
